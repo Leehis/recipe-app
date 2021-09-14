7f80f7636ce27d2859053d1b50587e6c0262c13b
Leehis <ehinomence@gmail.com>
2021-09-14 13:34:29 -0400

    Search function Operational

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import './App.css'
 const API_KEY = 'b7840a7ee1d04613b1f328f09de5c6eb'
 
 class App extends Component {
+  state = {
+    recipes: [],
+  }
+
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value
     console.log(recipeName)
     e.preventDefault()
     //if the api isn't called because the application is run from a local server then prefix the link with 'https:cors-anywhere.herokuapp.com/'
     const apiCall = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=${API_KEY}`
+      `https://api.spoonacular.com/recipes/complexSearch?query=${recipeName}&apiKey=${API_KEY}`
     )
 
     const data = await apiCall.json()
-    console.log(data)
+    this.setState({ recipes: data.results })
+    console.log(this.state.recipes)
   }
 
   render() {
@@ -25,6 +30,14 @@ class App extends Component {
           <h1 className='title'>Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        {this.state.recipes.map((recipe) => {
+          return (
+            <div key={recipe.id} className='items'>
+              <img src={recipe.image} alt={recipe.title} />
+              <p>{recipe.title}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
