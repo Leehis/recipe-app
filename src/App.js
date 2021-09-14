53b00ade1fe70031c2787001199befe548da46ba
Leehis <ehinomence@gmail.com>
2021-09-14 13:10:51 -0400

    Api call works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form'
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'b7840a7ee1d04613b1f328f09de5c6eb'
+
+class App extends Component {
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value
+    console.log(recipeName)
+    e.preventDefault()
+    //if the api isn't called because the application is run from a local server then prefix the link with 'https:cors-anywhere.herokuapp.com/'
+    const apiCall = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=${API_KEY}`
+    )
+
+    const data = await apiCall.json()
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <header className='appHeader'>
+          <h1 className='title'>Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
